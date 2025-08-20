0593c93cf5d8cb05d84326afe86230d31137685b
Ammar Atiq <ammar.pixako1@gmail.com>
2025-08-20 10:33:26 +0500

    created graphql resolvers and schemas

--- server/src/graphql/resolvers/resolvers.ts
+++ server/src/graphql/resolvers/resolvers.ts
@@ -1,6 +1,10 @@
+import { User } from "@/models/user";
+import { getAllUsers } from "@/controllers/user";
+
 export const graphQLResolver = {
   Query: {
     hello: () => "Hello world 1",
     hello2: () => "Hello world 2",
+    users: getAllUsers,
   },
 };
--- server/src/graphql/schema/schema.ts
+++ server/src/graphql/schema/schema.ts
@@ -1,5 +1,14 @@
 export const graphQLSchema = `#graphql
+   
+  type User {
+  _id:ID!,
+  name:String!,
+  email:String!,
+  password:String,
+      }
+
   type Query {
     hello: String,
     hello2: String
+    users:[User]
   }`;
