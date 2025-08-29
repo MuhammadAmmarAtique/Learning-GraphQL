8c5db1ceee483fac3d14ac3a86f067019ee47c2f
Ammar Atiq <ammar.pixako1@gmail.com>
2025-08-29 11:30:40 +0500

    added a  new controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,11 @@
 import { User } from "@/models/user";
 
 export const getAllUsers = async () => {
-    const users = await User.find();
-    return users;
+  const users = await User.find();
+  return users;
+};
+
+export const getUserbyId = async (parent: any, argument: { id: string }) => {
+  const user = await User.findById(argument.id);
+  return user;
 };
