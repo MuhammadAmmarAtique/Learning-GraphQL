82d3925a6e2e033edfd24efc57df2d3cb65a303e
Ammar Atiq <ammar.pixako1@gmail.com>
2025-08-29 18:03:58 +0500

    learned about mutations

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -11,3 +11,15 @@ export const getUserbyId = async (id: string) => {
   const user = await User.findById(id);
   return user;
 };
+
+export const createUser = async (
+  _parent: any,
+  args: { name: string; email: string; password?: string }
+) => {
+  const user = await User.create({
+    name: args.name,
+    email: args.email,
+    password: args.password,
+  });
+  return user;
+};
