ac0f3adcf476a56160fd95ed31e9ea83ec2c77a0
Ammar Atiq <ammar.pixako1@gmail.com>
2025-09-01 10:55:23 +0500

    cleaned code

--- server/src/middlewares/error.ts
+++ server/src/middlewares/error.ts
@@ -5,7 +5,6 @@ export const errorMiddleware = (
   err: ErrorHandler,
   req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   err.message ||= "Internal Server Error";
