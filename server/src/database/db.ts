fb171a94f62f18d93ff4b8b0ae7d0625ea7fa1fd
Ammar Atiq <ammar.pixako1@gmail.com>
2025-09-01 11:29:26 +0500

    improved string

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -5,7 +5,7 @@
     mongoose
       .connect(uri, { dbName: "LearningGraphQL" })
       .then((c) => {
-        console.log(`Connected with ${c.connection.name}`);
+        console.log(`Connected with ${c.connection.name} Database!`);
       })
       .catch((e) => console.log(`Err: during db connection through Mongoose ${e}`));
   
