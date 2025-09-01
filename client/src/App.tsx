1d142fd953841f04a1215dca197977996cc8fa2b
Ammar Atiq <ammar.pixako1@gmail.com>
2025-09-01 15:22:48 +0500

    learned about useLazyQuery

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,27 @@
 import "./App.css";
-import { useQuery } from "@apollo/client/react";
-import { gql } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client/react";
 import { getUsers } from "./graphql/queries/query";
 
+type User = { name: string };
+type GetUsersResponse = { users: User[] };
+
 function App() {
-  const { loading, data, error } = useQuery(
-    gql`
-      ${getUsers}
-    `
+  const [getUsersTrigger, { called, loading, data }] =
+    useLazyQuery<GetUsersResponse>(getUsers);
+
+  if (called && loading) return <p>Loading ...</p>;
+
+  if (!called) {
+    return <button onClick={() => getUsersTrigger()}>Load Users</button>;
+  }
+
+  return (
+    <div>
+      {data?.users?.map((user, idx) => (
+        <h1 key={idx}>Hello {user.name}!</h1>
+      ))}
+    </div>
   );
-  console.log("data coming after query", data);
-
-  if (error) return <h1>Err while Api call for getting data.</h1>;
-
-  return loading ? <p>Loading...</p> : <p>Test123</p>;
 }
 
 export default App;
