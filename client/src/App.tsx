42a21a0f8dd7f0e06af06137ae7b29c6be4b859e
Ammar Atiq <ammar.pixako1@gmail.com>
2025-09-01 15:06:10 +0500

    learned about useQuery hook of apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { useQuery } from "@apollo/client/react";
+import { gql } from "@apollo/client";
+import { getUsers } from "./graphql/queries/query";
 
 function App() {
-  return (
-    <>
-      <h1>My React App</h1>
-    </>
+  const { loading, data, error } = useQuery(
+    gql`
+      ${getUsers}
+    `
   );
+  console.log("data coming after query", data);
+
+  if (error) return <h1>Err while Api call for getting data.</h1>;
+
+  return loading ? <p>Loading...</p> : <p>Test123</p>;
 }
 
 export default App;
